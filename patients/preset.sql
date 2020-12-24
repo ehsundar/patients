ca60f973fa46624eb32b092d34e5d035c4c96502
Amir Ehsandar <ehsandaramir@gmail.com>
2020-12-24 13:42:33 +0330

    Add state table and preset values

--- patients/preset.sql
+++ patients/preset.sql
@@ -1,24 +1,54 @@
-INSERT INTO org (slug, name) VALUES ('pegah', 'Pegah Salamat');
+INSERT INTO org (slug, name)
+VALUES ('pegah', 'Pegah Salamat');
 
-INSERT INTO perm (slug, name) VALUES ('org-view', '');
-INSERT INTO perm (slug, name) VALUES ('org-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('org-view', '');
+INSERT INTO perm (slug, name)
+VALUES ('org-edit', '');
 
-INSERT INTO perm (slug, name) VALUES ('user-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('user-edit', '');
 
-INSERT INTO perm (slug, name) VALUES ('res-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('res-edit', '');
 
-INSERT INTO perm (slug, name) VALUES ('log-view', '');
+INSERT INTO perm (slug, name)
+VALUES ('log-view', '');
 
-INSERT INTO perm (slug, name) VALUES ('patient-all-view', '');
-INSERT INTO perm (slug, name) VALUES ('patient-all-edit', '');
-INSERT INTO perm (slug, name) VALUES ('patient-org-view', '');
-INSERT INTO perm (slug, name) VALUES ('patient-org-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('patient-all-view', '');
+INSERT INTO perm (slug, name)
+VALUES ('patient-all-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('patient-org-view', '');
+INSERT INTO perm (slug, name)
+VALUES ('patient-org-edit', '');
 
-INSERT INTO perm (slug, name) VALUES ('report-all-view', '');
-INSERT INTO perm (slug, name) VALUES ('report-all-edit', '');
-INSERT INTO perm (slug, name) VALUES ('report-org-view', '');
-INSERT INTO perm (slug, name) VALUES ('report-org-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('report-all-view', '');
+INSERT INTO perm (slug, name)
+VALUES ('report-all-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('report-org-view', '');
+INSERT INTO perm (slug, name)
+VALUES ('report-org-edit', '');
 
-INSERT INTO perm (slug, name) VALUES ('state-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('state-edit', '');
 
-INSERT INTO perm (slug, name) VALUES ('media-edit', '');
+INSERT INTO perm (slug, name)
+VALUES ('media-edit', '');
+
+INSERT INTO state (slug, name)
+VALUES ('not-attached', 'Not Attached');
+INSERT INTO state (slug, name)
+VALUES ('attached', 'Attached');
+INSERT INTO state (slug, name)
+VALUES ('detached', 'Detached');
+INSERT INTO state (slug, name)
+VALUES ('not-reported', 'Not Reported');
+INSERT INTO state (slug, name)
+VALUES ('report-ready', 'Report Ready');
+
+INSERT INTO state (slug, name)
+VALUES ('need-reattach', 'Need Reattach');
--- patients/schema.sql
+++ patients/schema.sql
@@ -1,11 +1,4 @@
 DROP TABLE IF EXISTS org CASCADE;
-DROP TABLE IF EXISTS perm CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS perm_linker CASCADE;
-DROP TABLE IF EXISTS patient CASCADE;
-DROP TABLE IF EXISTS res CASCADE;
-DROP TABLE IF EXISTS report CASCADE;
-
 CREATE TABLE org
 (
     slug       VARCHAR(32) PRIMARY KEY,
@@ -14,6 +7,7 @@ CREATE TABLE org
     name       VARCHAR(32)
 );
 
+DROP TABLE IF EXISTS perm CASCADE;
 CREATE TABLE perm
 (
     slug       VARCHAR(32) PRIMARY KEY,
@@ -22,6 +16,7 @@ CREATE TABLE perm
     name       VARCHAR(32)
 );
 
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users
 (
     username   VARCHAR(32) PRIMARY KEY,
@@ -33,6 +28,7 @@ CREATE TABLE users
     FOREIGN KEY (org) REFERENCES org (slug)
 );
 
+DROP TABLE IF EXISTS perm_linker CASCADE;
 CREATE TABLE perm_linker
 (
     id         SERIAL PRIMARY KEY,
@@ -44,6 +40,7 @@ CREATE TABLE perm_linker
     FOREIGN KEY (perm) REFERENCES perm (slug)
 );
 
+DROP TABLE IF EXISTS patient CASCADE;
 CREATE TABLE patient
 (
     id           SERIAL PRIMARY KEY,
@@ -57,6 +54,7 @@ CREATE TABLE patient
     FOREIGN KEY (creator_user) REFERENCES users (username)
 );
 
+DROP TABLE IF EXISTS res CASCADE;
 CREATE TABLE res
 (
     id         SERIAL PRIMARY KEY,
@@ -67,6 +65,14 @@ CREATE TABLE res
     cap        INTEGER
 );
 
+DROP TABLE IF EXISTS state CASCADE;
+CREATE TABLE state
+(
+    slug VARCHAR(32) PRIMARY KEY,
+    name VARCHAR(32)
+);
+
+DROP TABLE IF EXISTS report CASCADE;
 CREATE TABLE report
 (
     id           SERIAL PRIMARY KEY,
@@ -74,6 +80,10 @@ CREATE TABLE report
 
     patient      INTEGER,
     creator_user VARCHAR(32),
+    res          INTEGER,
+    state        VARCHAR(32),
     FOREIGN KEY (patient) REFERENCES patient (id),
-    FOREIGN KEY (creator_user) REFERENCES users (username)
+    FOREIGN KEY (creator_user) REFERENCES users (username),
+    FOREIGN KEY (res) REFERENCES res (id),
+    FOREIGN KEY (state) REFERENCES state (slug)
 );
